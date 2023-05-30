4c2a43ffb40605dde9b72b001693c99caa02ee81
JJ Style <style.jj@pm.me>
2023-05-30 23:06:46 +0100

    format and add comments

--- ebay-all-prices.user.js
+++ ebay-all-prices.user.js
@@ -7,23 +7,35 @@
 // @match        https://www.ebay.co.uk/itm/*
 // @grant        none
 // ==/UserScript==
+
+// get page source & find where the item JSON is in one of the script blocks
 var generatedSource = new XMLSerializer().serializeToString(document);
 const re2 = new RegExp("itmVarModel");
 let matches = re2.exec(generatedSource);
 let hitIndex = matches.index;
-let json2end = generatedSource.substr(hitIndex + 13);
+let json2end = generatedSource.substring(hitIndex + 13);
 
+// iterate through string until end of json content
+// and then parse to json object
 let idx = 0;
 let depth = 0;
-do{if (json2end[index] == '{') {depth++;} else if (json2end[index] == '}') {depth--;} index++;} while (depth > 0 && index <= json2end.length);
-let data = JSON.parse(json2end.substr(0, index));
+do {
+  if (json2end[index] == "{") {
+    depth++;
+  } else if (json2end[index] == "}") {
+    depth--;
+  }
+  index++;
+} while (depth > 0 && index <= json2end.length);
+let data = JSON.parse(json2end.substring(0, index));
 
-var el = document.createElement('ul');
-Object.values(j.itemVariationsMap).forEach(item => {
-let li = document.createElement("li");
+// create a list of prices & put in document under the main price
+var el = document.createElement("ul");
+Object.values(j.itemVariationsMap).forEach((item) => {
+  let li = document.createElement("li");
   li.innerText = item.price;
   el.appendChild(li);
 });
 
-let priceContent = document.getElementsByClassName('x-bin-price__content')[0];
+let priceContent = document.getElementsByClassName("x-bin-price__content")[0];
 priceContent.appendChild(el);
